5e02c43be27a89a58cb5dac7c5a9b01f2c0443cb
vrnranjan22 <vrnranjan22@gmail.com>
2020-08-13 11:11:41 +0530

    Weather app completed

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,19 @@
-console.log('Client side javascript file is loaded!')
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const msg1 = document.getElementById('res-1');
+const msg2 = document.getElementById('res-2');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value;
+  msg1.textContent = 'Loading...';
+  msg2.textContent = '';
+  axios(`http://localhost:5000/weather?address=${location}`).then((res) => {
+    if (res.data.error) {
+      msg1.textContent = res.data.error;
+    } else {
+      msg1.textContent = res.data.locations;
+      msg2.textContent = res.data.forecast;
+    }
+  });
+});
